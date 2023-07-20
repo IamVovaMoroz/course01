5c0d20a9ff65a3681178acb912f17d3887575281
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-07-20 16:40:38 +0200

    ok

---
+++ mongoose/app.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+
+
+const DB_HOST = "mongodb+srv://VolodymyrMoroz:NOni01041983@cluster0.oyfrio4.mongodb.net/books_base"
+
+mongoose.set('strictQuery', true)
+
+mongoose.connect(DB_HOST).then(()=> console.log("Database connect success")).catch(error => console.log(error.message))
