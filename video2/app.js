17e796ce1ee411a154c82fd39c3e773f80c0b338
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-08-16 16:18:31 +0200

    консольный додаток, чтение базы и функции

--- video2/app.js
+++ video2/app.js
@@ -1,1 +1,85 @@
 const books = require("./books/index")
+// для создания консольного додатка
+const yargs = require('yargs')
+const {hideBin} = require('yargs/helpers')
+
+const invokeAction = async ({action, id, title, author}) =>{
+    switch(action) {
+        // вызывается этот метод getAll , который считывает json 
+      case  "getAll":
+const allBooks = await books.getAll()
+return console.log(allBooks)
+case  "getById":
+    // вызываем функию которая ищет по ид и перезаем тут наше ид при вызове функции
+    const oneBook = await books.getById(id)
+    return console.log(oneBook) 
+
+case "addBook":
+    // функция ожидает  title, author с массива книг и записывает в переменную
+    const newBook = await books.addBook({title, author})
+    return console.log(newBook)
+
+    case "updateById":
+    // функция ожидает  title, author с массива книг и записывает в переменную
+    const updateBook = await books.updateById(id, {title, author})
+    return console.log(updateBook)
+
+    case "deleteById":
+        // функция ожидает  title, author с массива книг и записывает в переменную
+        const deleteBook = await books.deleteById(id)
+        return console.log(deleteBook)
+default:
+    return console.log("unknow action")
+    }
+
+    
+}
+//1) при вызове передаем название действия
+// invokeAction({action: "read"})
+//2) поиск книги по ид
+// invokeAction({action: "getById", id: "ckncksclsclsc"})
+
+//3) добавить книгу. Название действия и что сделать(добавить название и автора) 
+// action: "addBook" и case "addBook": может быть любой
+// invokeAction({action: "addBook", title: "New Book", author: "Daniel M"})
+
+// 4) обновить книгу PUT запрос, перезаписывает всё - передаем все поля поэтому, что изменить и старые
+// invokeAction({action: "updateById",  id: "ckncksclsclsc", title: "New Update Book", author: "Daniel Mo"})
+
+//5) delete by id
+
+// invokeAction({action: "deleteById",  id: "ckncksclsclsc"})
+
+// ++++++++++++++++++++ СОЗДАНИЕ КОНСОЛЬНОГО ПРИЛОЖЕНИЯ ЧЕРЕЗ process.argv 1 СПОСОБ +++++++++++++++++++
+
+
+// для работы через командную строку используем process.argv и делаем консольный додаток
+// смотрим есть ли в массиве --action
+
+// const actionIndex = process.argv.indexOf("--action")
+
+// // ищем действие после --action
+// if(actionIndex !== -1){
+//     const action = process.argv[actionIndex + 1]
+    
+//     console.log(action) // => getAll  т.е прочитало 2 значение после action. node app --action getAll
+
+//     // Передаем это 2 значение Case в функцию т.е что в терминале 
+//     invokeAction({action})
+//     // теперь терминал работает node app --action getAll
+// }
+
+
+// +++++++++++++++++++ СОЗДАНИЕ КОНСОЛЬНОГО ПРИЛОЖЕНИЯ ЧЕРЕЗ yargs 2 СПОСОБ +++++++++++++++++++++++++
+
+// для создания консольного додатка используем npm i yargs
+
+const arr = hideBin(process.argv)  // в функцию hideBin закидываем наш (process.argv)
+
+// console.log(arr)  // возвращает [ '--action', 'getAll' ] при вводе в терминале node app --action getAll !!! т.е отрезал 2 элемента массива!!!
+
+const {argv} = yargs(arr) // передаем в yargs наш массив и он делает action: 'getAll' т.е ключём, а getAll - значением этого ключа 
+
+// console.log(argv) // получаем это и можем передать в функцию { _: [], action: 'getAll', '$0': 'app' }
+// Но он воспринимает ид цифры как число и не находит, поэтому в коде даписываем при получении bookId в стринг
+invokeAction(argv)
--- video2/books/index.js
+++ video2/books/index.js
@@ -1,10 +1,96 @@
+// index.js - метод для получения книг из json и экспортируем этот метод(функйию)
+// нужно прочитать json file
 
-// index.js - метод для получения книг из json и экспортируем этот метод(функйию) 
+const fs = require('fs').promises
+// для получения абсолютного пути
+const path = require('path')
 
-const getAllBooks = () => {
+const { nanoid } = require('nanoid')
 
+// const booksPath = path.join(__dirname, "", "books.json" )
+const booksPath = path.join(__dirname, 'books.json')
+
+// console.log(booksPath)
+
+const getAll = async () => {
+  const data = await fs.readFile(booksPath, 'utf-8')
+  // т.к JSON.parse(data) тоже делает что и "utf-8", можно не писать это
+  // const data = await fs.readFile(booksPath)
+  // возвращаем текст распарсенный
+  return JSON.parse(data)
+}
+
+const getById = async id => {
+  // получаем все книги
+  const books = await getAll()
+
+  const bookId = String(id)
+  // ищем с нужным ид, что прилетел
+  const result = books.find(item => item.id === bookId)
+  // чтобы не возращалась undefined , то результат или null
+  return result || null
+}
+
+// создаем функцию добавления книши с ИД
+const addBook = async data => {
+  // получаем весь массив in books
+  const books = await getAll()
+
+  const newBook = {
+    // add id
+    id: nanoid(),
+    // данные распыляем data(title , author)
+    ...data
+  }
+  // в массив добавляем нокую книгу
+  books.push(newBook)
+  // перезаписать json используя метод writeFile
+  // JSON.stringify перезапишет в 1 длинную строку JSON а JSON.stringify(books, null, 2)  переводит в норм
+  await fs.writeFile(booksPath, JSON.stringify(books, null, 2))
+  return newBook
+}
+
+const updateById = async (id, data) => {
+  // получаем все книги
+  const books = await getAll()
+  const bookId = String(id)
+  const index = books.findIndex(item => item.id === bookId)
+
+  // const index = books.findIndex(item => item.id === id) при использовании yargs меняем (item => item.id === bookId)
+  // если в базе не найдёт, придёт 1
+  if (index === -1) {
+    // console.log("Книга не найдена");
+    return null
+  }
+  // перезаписываем книгу
+  books[index] = { id, ...data }
+  // перезаписываем JSON
+  await fs.writeFile(booksPath, JSON.stringify(books, null, 2))
+  return books[index]
+}
+
+const deleteById = async id => {
+    // получаем все книги
+  const books = await getAll()
+  // переводим ид в строку для работы через командную строку
+  const bookId = String(id)
+  const index = books.findIndex(item => item.id === bookId)
+  // если в базе не найдёт, придёт 1
+  if (index === -1) {
+    // console.log("Книга не найдена");
+    return null
+  }
+//   splice удалит и вернёт массив того что он вырезал. т.е получаем удалённую книгу
+  const [result] = books.splice(index, 1)
+//   перезаписываем
+await fs.writeFile(booksPath, JSON.stringify(books, null, 2))
+return result
 }
 
 module.exports = {
-    getAllBooks
+  getAll,
+  getById,
+  addBook,
+  updateById,
+  deleteById
 }
